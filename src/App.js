af1508e44eb14a61213baffd540a17f0c3e64b75
Edouard Michel <MathMinor@users.noreply.github.com>
2017-04-17 17:33:34 -0400

    Update Carousel image size and add text

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Navbar from './Components/Navbar';
 import Carousel from './Components/Carousel';
+import Info from './Components/Info'
 import './App.css';
 
 class App extends Component {
@@ -9,6 +10,7 @@ class App extends Component {
       <div className="App">
         <Navbar />
         <Carousel />
+        <Info />
 
       </div>
     );
---
+++ src/Components/Info.js
@@ -0,0 +1,27 @@
+import React, {Component} from 'react';
+
+class Info extends Component{
+  render(){
+    return(
+      <div className="info">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc elit
+          dui, mattis nec odio suscipit, ultrices euismod libero. In iaculis
+          finibus nulla eu sodales. Mauris vitae arcu scelerisque, pellentesque
+          lectus ornare, pellentesque nisi. Duis id velit ut elit congue
+          porttitor sed eu enim. Nunc accumsan sagittis vestibulum. Nullam sed
+          eros vel lectus dapibus mattis. Quisque ut nunc at ex tempor fringilla.
+          Praesent dictum imperdiet eros porta ultricies. Suspendisse
+          ullamcorper elit eu justo aliquam volutpat. Curabitur id condimentum
+          nunc. Aliquam erat volutpat. Cras ipsum enim, mollis ut aliquet sit
+          amet, finibus at ligula. Sed lobortis fermentum gravida. Nulla id
+          imperdiet dolor. Sed dignissim porttitor orci, non blandit tellus
+          feugiat pharetra. Integer ultrices libero at hendrerit molestie.
+        </p>
+        <p><a href="https://github.com/MathMinor/crowdsourcing"><i className="fa fa-github fa-2x fa-fw" aria-hidden="true"></i> </a></p>
+      </div>
+    )
+  }
+}
+
+export default Info;
