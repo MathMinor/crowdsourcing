5f05c367c37b08248ccfb891622ec9dd6ee5ad04
Edouard Michel <MathMinor@users.noreply.github.com>
2017-05-08 22:01:14 -0400

    Store user input in an array

--- src/Components/Chat/Messages.js
+++ src/Components/Chat/Messages.js
@@ -5,15 +5,23 @@ class Messages extends Component {
     super(props);
 
     this.state = {
-     newMessage: ''
+     newMessage: []
    };
 
   }
-  
+
+  componentWillReceiveProps(nextProps){
+    let messages = nextProps.newMessage;
+    this.state.newMessage.push(messages);
+    this.setState({newMessage: this.state.newMessage});
+  }
+
   render(){
+    console.log(this.state.newMessage);
+    console.log(this.props.newMessage);
     return(
       <div>
-        <ul id="messages">{this.props.example}</ul>
+        <ul id="messages">{this.props.newMessage}</ul>
       </div>
     )
   }
