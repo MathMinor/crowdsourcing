fff1a3ff4f127b17e69efb207494fad81e91f9c6
Edouard Michel <MathMinor@users.noreply.github.com>
2017-05-23 22:15:53 -0400

    Use form tag in Chatroom Component

--- src/Components/Chat/Chatroom.js
+++ src/Components/Chat/Chatroom.js
@@ -6,7 +6,7 @@ class Chatroom extends Component {
   constructor() {
     super();
     this.socket=io("http://localhost:5000")
-    this.onSubmit=this.onClick.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
 
     this.state = {
       newMessage: ''
@@ -14,22 +14,23 @@ class Chatroom extends Component {
 
   }
 
-  onClick(e) {
+  handleSubmit(e) {
     let messageInput = document.getElementById("m");
     const currMessage = messageInput.value;
     messageInput.value= "";
     this.socket.emit('chat message', currMessage);
     this.setState({newMessage: currMessage});
+    e.preventDefault();
     return false;
   }
 
   render(){
     return(
       <div>
-        <Messages newMessage={this.state.newMessage}/>
-        <div className="chatForm">
-          <input id="m" autoComplete="off" /><button className="coolButton" onClick={(e) => this.onClick(e)}>Send</button>
-        </div>
+        <Messages newMessage={this.state.newMessage} />
+        <form onSubmit={this.handleSubmit}>
+          <input id="m" autoComplete="off" /><button>Send</button>
+        </form>
       </div>
     )
   }
